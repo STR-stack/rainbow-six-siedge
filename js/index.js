cb973aa342a6dc5b52eb29ea7e52f445cb35e071
STR-stack <maxyu2000@mail.ru>
2025-05-18 17:29:54 +0300

    Кое что сделал

--- js/index.js
+++ js/index.js
@@ -84,3 +84,33 @@ function initGallery(){
 }
 
 document.addEventListener('DOMContentLoaded', initGallery)
+
+
+function initPlayer(){
+    const btn = document.querySelector(".music-track-round")
+    const audio = document.getElementById("audioPlayer")
+
+    btn.addEventListener("click" () => {
+        if (audio.paused){
+            audio.play()
+            btn.innerHTML = "<i class ='fas fa-pause'></i>"
+        }else{
+            audio.pause()
+            btn.innerHTML = "<i class ='fas fa-play'></i>"
+        }
+        
+    })
+
+    const time = document.querySelector('.time')
+    audio.addEventListener("timesuPdate", () => {
+        let minutes = Math.floor(audio.currentTime / 60)
+        let seconds = Math.floor(audio.currentTime % 60).toString().padStart(2, "0")
+        let maxMinutes = Math.floor(audio.duration / 60)
+        let maxSeconds = Math.floor(audio.duration / 60).toString().padStart(2, "0")
+
+         
+        time.innerHTML = `${minutes}:${seconds} / ${maxMinutes}:${maxSeconds}`
+    })
+}
+
+document.addEventListener("DOMContentLoader", initPlayer)
